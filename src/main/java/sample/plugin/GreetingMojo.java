412075573df534164ac65795673eea28ca8339b1
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-11-07 19:09:53 +0000

    first committal

---
+++ src/main/java/sample/plugin/GreetingMojo.java
@@ -0,0 +1,14 @@
+package sample.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Says "Hi" to the user.
+ * @goal sayhi
+ */
+public class GreetingMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException {
+        getLog().info("Hello, world.");
+    }
+}
