ed3093b9cb7f30eb34c6437bebea14ffc6f13e77
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-11-07 21:23:46 +0000

    error messages

--- src/main/clojure/mvn_aa_plugin/mojo.clj
+++ src/main/clojure/mvn_aa_plugin/mojo.clj
@@ -2,10 +2,11 @@
   (:gen-class 
   		:name    "mvn_aa_plugin.mojo" 
   		:extends "org.apache.maven.plugin.AbstractMojo"
-  		:prefix  "mojo-"))
+  		:prefix  "mojo-")
+  (:require [mvn-aa-plugin.aa :as aa]))
 
 (defn- info [this msg]
   (.info (.getLog this) msg))
 
 (defn mojo-execute [this]
-  (info this "HELLO BEAUTIFUL WORLD"))
+  (info this aa/default-message))
