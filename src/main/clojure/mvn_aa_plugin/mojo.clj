0a1b542ba040770c3ebba29b8542fc8edef39586
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-11-07 21:07:01 +0000

    Finally a plugin in purest clojure!

--- src/main/clojure/mvn_aa_plugin/mojo.clj
+++ src/main/clojure/mvn_aa_plugin/mojo.clj
@@ -1,5 +1,11 @@
 (ns mvn-aa-plugin.mojo
-  (:gen-class))
+  (:gen-class 
+  		:name    "mvn_aa_plugin.mojo" 
+  		:extends "org.apache.maven.plugin.AbstractMojo"
+  		:prefix  "mojo-"))
 
-(defn printy []
-  (println "HELLO WORLDY"))
+(defn- info [this msg]
+  (.info (.getLog this) msg))
+
+(defn mojo-execute [this]
+  (info this "HELLO BEAUTIFUL WORLD"))
---
+++ src/main/java/sample/plugin/ShimMojo.java
@@ -0,0 +1,12 @@
+//package sample.plugin;
+//
+//import org.apache.maven.plugin.AbstractMojo;
+//import org.apache.maven.plugin.MojoExecutionException;
+//
+//public class ShimMojo extends AbstractMojo {
+//
+//    public void execute() throws MojoExecutionException {
+//        getLog().info("aaa");
+//    }
+//    
+//}
