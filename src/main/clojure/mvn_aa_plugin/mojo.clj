fb691c15a37cf8b584e38556b9461c06ae71da01
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-11-07 21:53:57 +0000

    added message param, tidy ascii-art

--- src/main/clojure/mvn_aa_plugin/mojo.clj
+++ src/main/clojure/mvn_aa_plugin/mojo.clj
@@ -1,12 +1,20 @@
 (ns mvn-aa-plugin.mojo
+
   (:gen-class 
   		:name    "mvn_aa_plugin.mojo" 
   		:extends "org.apache.maven.plugin.AbstractMojo"
-  		:prefix  "mojo-")
+  		:prefix  "mojo-"
+  		:methods [[setMessage [String] void]])
+
   (:require [mvn-aa-plugin.aa :as aa]))
+
+(def params (atom {:message "ascii-art maven plugin"}))
 
 (defn- info [this msg]
   (.info (.getLog this) msg))
 
+(defn mojo-setMessage [this name]
+  (swap! params assoc :message name))
+
 (defn mojo-execute [this]
-  (info this aa/default-message))
+  (info this (str aa/default-message "\n\n    " (@params :message) "\n")))
